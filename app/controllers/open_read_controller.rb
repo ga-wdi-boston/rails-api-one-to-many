27fafea2d9f2a34426f5911a170e3684cb4a2787
berziiii <berzelba@gmail.com>
2016-09-22 15:46:03 -0400

    Update from talk-template
    Remove unnecessary files and update files from template

--- app/controllers/open_read_controller.rb
+++
@@ -1,6 +0,0 @@
-# Allow unauthenticate access to read actions
-class OpenReadController < ProtectedController
-  READ_ACTIONS = [:index, :show].freeze
-  skip_before_action :authenticate, only: READ_ACTIONS
-  before_action :set_current_user, only: READ_ACTIONS
-end
